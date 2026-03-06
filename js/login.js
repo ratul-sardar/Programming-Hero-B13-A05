830e333dd8a0898bb6469d23bc1068400253e6b4
ratul-sardar <ratulsardar4747@gmail.com>
2026-03-06 22:54:32 +0600

    added the login.js file and now with correct pass and username the page redirects to the dashboard

---
+++ js/login.js
@@ -0,0 +1,8 @@
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+function login() {
+  if (username.value === "admin" && password.value === "admin123") {
+    window.location.pathname = "../dashboard.html";
+  }
+}
