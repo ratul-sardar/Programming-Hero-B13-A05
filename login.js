537961edf06a81b037fb09e57b42da47bc61962d
ratul-sardar <ratulsardar4747@gmail.com>
2026-03-09 09:18:51 +0600

    null

--- login.js
+++ login.js
@@ -3,6 +3,6 @@ const password = document.getElementById("password");
 
 function login() {
   if (username.value === "admin" && password.value === "admin123") {
-    window.location.pathname = "../dashboard.html";
+    window.location.pathname = "./dashboard.html";
   }
 }
